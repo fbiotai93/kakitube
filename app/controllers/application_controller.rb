98ccde66ae5fb962e413682f1878992d3e68adc7
AmirolAhmad <9w2pdf@gmail.com>
2015-04-16 00:17:18 +0800

    code test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	protected
-
+	
 	def configure_permitted_parameters
 		devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
 		devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:login, :username, :email, :password, :remember_me) }
