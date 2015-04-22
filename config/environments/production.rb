61776dfcc21067b6ef0d23599219b9a8b47dff65
AmirolAhmad <9w2pdf@gmail.com>
2015-04-23 03:45:11 +0800

    fixed config assets path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,10 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
+
+  config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+  config.assets.precompile += %w( .svg .eot .woff .woff2 .ttf)
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
