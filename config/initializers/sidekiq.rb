c2a10d427ab19daf690e2800a5c26b60d30e8bef
amirolahmad <9w2pdf@gmail.com>
2015-05-14 23:29:38 +0800

    tune concurrency n pool

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_server do |config|
-  config.redis = { :url => "redis://#{Settings.sidekiq}:6379" }
+  config.redis = { :url => "redis://#{Settings.sidekiq}:6379", namespace: 'kakitube' }
 end
 Sidekiq.configure_client do |config|
-  config.redis = { :url => "redis://#{Settings.sidekiq}:6379" }
+  config.redis = { :url => "redis://#{Settings.sidekiq}:6379", namespace: 'kakitube' }
 end
