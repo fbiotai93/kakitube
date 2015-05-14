1c663aa0cd9678be79b1b7304299b85845e905de
amirolahmad <9w2pdf@gmail.com>
2015-05-14 12:07:29 +0800

    change production name to temporary use beta

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,12 +1,18 @@
+set :application, 'kakitube_production'
+set :stage, :production
+set :rails_env, :production
+set :deploy_to, '/home/deploy/kakitube_production'
+set :release_note_url, 'http://beta.kakitube.com/release_note.txt'
+
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+# role :app, %w{deploy@example.com}
+# role :web, %w{deploy@example.com}
+# role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
@@ -14,7 +20,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+# server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
