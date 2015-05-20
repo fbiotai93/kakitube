60a8d523241cf70501de205a0a75e64045c6f0a8
amirolahmad <9w2pdf@gmail.com>
2015-05-21 02:42:23 +0800

    secure and refactor api

--- app/controllers/api/v1/pages_controller.rb
+++ app/controllers/api/v1/pages_controller.rb
@@ -6,25 +6,25 @@ module Api
       # api/v1/movies
       def movies
         @posts = Post.all
-        respond_with @posts.to_json(except: [:user_id])
+        respond_with @posts.to_json(include: [:genre], except: [:user_id, :genre_id])
       end
 
       # api/v1/tv-series
       def series
         @siris = Siri.all
-        respond_with @siris.to_json(include: [seasons: {include: [:episodes]}], except: [:user_id])
+        respond_with @siris.to_json(include: [:siri_genre, seasons: {include: [episodes: { except: [:season_id] }], except: [:siri_id]}], except: [:user_id, :siri_genre_id])
       end
 
       #api/v1/movies/movie-name
       def show
         @post = Post.find(params[:id])
-        respond_with @post.to_json(include: [:genre],except: [:user_id, :genre_id])
+        respond_with @post.to_json(include: [:genre], except: [:user_id, :genre_id])
       end
 
       #api/v1/tv-series/series-name
       def view
         @siri = Siri.find(params[:id])
-        respond_with @siri.to_json(include: [:siri_genre, seasons: {include: [:episodes]}], except: [:user_id, :siri_genre_id])
+        respond_with @siri.to_json(include: [:siri_genre, seasons: {include: [episodes: { except: [:season_id] }], except: [:siri_id]}], except: [:user_id, :siri_genre_id])
       end
 
     end
