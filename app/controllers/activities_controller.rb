6a5e4fd9ea281db6eb275bd746f0b1d0cd05d551
amirolahmad <9w2pdf@gmail.com>
2015-05-24 22:22:55 +0800

    add total post at activities page

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -3,6 +3,9 @@ class ActivitiesController < ApplicationController
   before_filter :admin_only
 
   def index
-    @activities = PublicActivity::Activity.order("created_at desc").paginate(:page => params[:page], :per_page => 30)
+    @activities = PublicActivity::Activity.order("created_at DESC").paginate(:page => params[:page], :per_page => 30)
+    @admins = User.where(admin: 1)
+    @post = Post.all
+    @siri = Siri.all
   end
 end
