9bacd7f209664b29a8ec2779464364e9a74123df
amirolahmad <9w2pdf@gmail.com>
2015-07-25 08:53:30 +0800

    change from uploads s3 to local

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,14 +1,14 @@
-CarrierWave.configure do |config|
-  config.storage = :fog
-	config.fog_credentials = {
-		:provider => 'AWS',
-		:aws_access_key_id => Settings.s3.access_key_id,
-		:aws_secret_access_key => Settings.s3.secret_access_key,
-		:region	=> Settings.s3.region
-	}
-	config.fog_directory = Settings.s3.bucket # required
-	config.fog_public = true # optional, defaults to true
-	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} # optional, defaults to {}
-	config.asset_host = Settings.image_host
-	config.cache_dir = "#{Rails.root}/tmp/uploads"
-end 
+# CarrierWave.configure do |config|
+#   config.storage = :fog
+# 	config.fog_credentials = {
+# 		:provider => 'AWS',
+# 		:aws_access_key_id => Settings.s3.access_key_id,
+# 		:aws_secret_access_key => Settings.s3.secret_access_key,
+# 		:region	=> Settings.s3.region
+# 	}
+# 	config.fog_directory = Settings.s3.bucket # required
+# 	config.fog_public = true # optional, defaults to true
+# 	config.fog_attributes = {'Cache-Control'=>'max-age=315576000'} # optional, defaults to {}
+# 	config.asset_host = Settings.image_host
+# 	config.cache_dir = "#{Rails.root}/tmp/uploads"
+# end 
