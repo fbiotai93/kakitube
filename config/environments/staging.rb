ee7a01eba17eb901c82e5f086677ddc61817d940
Amirol Ahmad <9w2pdf@gmail.com>
2015-11-20 14:34:15 +0800

    off cache for staging

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
-  config.cache_store = :dalli_store
+  config.action_controller.perform_caching = false
+  # config.cache_store = :dalli_store
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
@@ -91,5 +91,5 @@ Rails.application.configure do
     :domain         => 'staging.kakitube.com',
     :enable_starttls_auto => true
   }
-  
+
 end
